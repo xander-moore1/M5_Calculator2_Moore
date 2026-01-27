2da175fadded310bfc2235fc943d1361a1408b69
XJM <xander_moore1@mymail.eku.edu>
2026-01-27 11:26:56 -0500

    Initial Commit

---
+++ M2_Calculator_Moore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2_Calculator_Moore
+//
+//  Created by Moore, Xander J. on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
