ba2983e47e0becb03abba939197e0e674763e32a
XJM <xander_moore1@mymail.eku.edu>
2026-02-17 11:46:28 -0500

    Base version

--- M2_Calculator_Moore/ViewController.swift
+++ M2_Calculator_Moore/ViewController.swift
@@ -8,7 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var operandTextFieldA: UITextField!
+    @IBOutlet weak var operandTextFieldB: UITextField!
+    @IBOutlet weak var operatorButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func operatorPressed(_ sender: Any) {
+        let actionSheet = UIAlertController(title:nil, message:nil, preferredStyle: .alert);
+        
+        let plusAction = UIAlertAction(title: "+", style: .default) {_ in
+            self.operatorButton.titleLabel?.text = "+";
+        }
+        
+        actionSheet.addAction(plusAction);
+        
+        let minusAction = UIAlertAction(title: "-", style: .default) {_ in
+            self.operatorButton.titleLabel?.text = "-";
+        }
+        
+        actionSheet.addAction(minusAction);
+        
+        let timesAction = UIAlertAction(title: "*", style: .default) {_ in
+            self.operatorButton.titleLabel?.text = "*";
+        }
+        
+        actionSheet.addAction(timesAction);
+        
+        let divideAction = UIAlertAction(title: "/", style: .default) {_ in
+            self.operatorButton.titleLabel?.text = "/";
+        }
+        
+        actionSheet.addAction(divideAction);
+        
+        self.present(actionSheet, animated: true);
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        let op1 = Int(operandTextFieldA.text!);
+        let op2 = Int(operandTextFieldB.text!);
+        let op = operatorButton.titleLabel?.text;
+        
+        if (op == "+") {
+            let result = op1! + op2!;
+            resultLabel.text = "\(result)";
+        }
+        else if (op == "-") {
+            let result = op1! - op2!;
+            resultLabel.text = "\(result)";
+        }
+        else if (op == "*") {
+            let result = op1! * op2!;
+            resultLabel.text = "\(result)";
+        }
+        else if (op == "/") {
+            let result = op1! / op2!;
+            resultLabel.text = "\(result)";
+        }
+        else {
+            resultLabel.text = "Invalid operator";
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
